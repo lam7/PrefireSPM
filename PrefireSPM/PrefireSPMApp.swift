9e4d5242af4d1def8e02b149c2aba84ce1f07bf8
lam7 <okahiroki@team-lab.com>
2025-02-13 18:26:27 +0900

    initialize

---
+++ PrefireSPM/PrefireSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrefireSPMApp.swift
+//  PrefireSPM
+//
+//  Created by 岡 宙生 on 2025/02/13.
+//
+
+import SwiftUI
+
+@main
+struct PrefireSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
